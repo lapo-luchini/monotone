66f976b0cab49ed4fa8cc9a150b631f5d2c9a525
Timothy Brownawell <tbrownaw@gmail.com>
2006-06-09 22:57:53 +0000

    more tests (250/346); some minor changes

--- tests/importing_CVS_files/__driver__.lua
+++ tests/importing_CVS_files/__driver__.lua
@@ -1,14 +1,10 @@
 
+include("/common/cvs.lua")
 mtn_setup()
 
-skip_if(not existsonpath("cvs"))
-
 writefile("importme.0", "version 0 of test file")
-
 writefile("importme.1", "version 1 of test file")
-
 writefile("importme.2", "version 2 of test file")
-
 writefile("importme.3", "version 3 of test file")
 
 tsha0=sha1("importme.0")
@@ -18,16 +14,7 @@ tsha3=sha1("importme.3")
 
 -- build the cvs repository
 
-cvsroot = test.root .. "/cvs-repository"
-
-function cvs(...)
-  return {"cvs", "-d", cvsroot, unpack(arg)}
-end
-
-check(cvs("-q", "init"), 0, false, false)
-check(exists(cvsroot))
-check(exists(cvsroot .. "/CVSROOT"))
-check(exists(cvsroot .. "/CVSROOT/modules"))
+cvs_setup()
 
 -- check out the workspace and make some commits
 -- note that this has to use copyfile, rather than rename, to update
--- tests/importing_a_CVS_file_with_one_version/__driver__.lua
+++ tests/importing_a_CVS_file_with_one_version/__driver__.lua
@@ -1,5 +1,5 @@
 
-skip_if(not existsonpath("cvs"))
+include("/common/cvs.lua")
 mtn_setup()
 
 writefile("importme.0", "version 0 of test file")
@@ -8,16 +8,7 @@ tsha = sha1("importme.0")
 
 -- build the cvs repository
 
-cvsroot = test.root .. "/cvs-repository"
-
-function cvs(...)
-  return {"cvs", "-d", cvsroot, unpack(arg)}
-end
-
-check(cvs("-q", "init"), 0, false, false)
-check(exists(cvsroot))
-check(exists(cvsroot .. "/CVSROOT"))
-check(exists(cvsroot .. "/CVSROOT/modules"))
+cvs_setup()
 
 -- check out the workspace and make a commit
 
