70d42f466dfedbfc75404027b041b863d0b4dae4
Thomas Keller <me@thomaskeller.biz>
2011-03-21 00:13:01 +0000

    Add temporary pageflip / teaser image to the upper right pointing at the 1.0 release

--- header.inc.php
+++ header.inc.php
@@ -8,8 +8,59 @@
 <![endif]-->
 <link rel="alternate" type="application/rss+xml" title="monotone news" href="news.xml.php" />
 <link rel="alternate" type="application/rss+xml" title="monotone releases" href="releases.xml.php" />
+<script type="text/javascript" src="https://www.google.com/jsapi?key=ABQIAAAA19yJy5ZKGCq-pdY8NMZbrxQBE22dz7hS6Lv_ovduzW42XUZpZhQ-hDriybitASPo1d8A770Y_f5hug"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+    $("#pageflip").hover(function() {
+        $("#pageflip img , .msg_block").stop()
+            .animate({
+                width: '305px',
+                height: '317px'
+            }, 500);
+        } , function() {
+        $("#pageflip img").stop()
+            .animate({
+                width: '50px',
+                height: '52px'
+            }, 220);
+        $(".msg_block").stop()
+            .animate({
+                width: '50px',
+                height: '50px'
+            }, 200);
+    });
+});
+</script>
+<style type="text/css">
+#pageflip {
+    position: relative;
+}
+#pageflip img {
+    width: 50px; height: 52px;
+    z-index: 99;
+    position: absolute;
+    right: 0; top: 0;
+    -ms-interpolation-mode: bicubic;
+}
+#pageflip .msg_block {
+    width: 50px; height: 50px;
+    position: absolute;
+    z-index: 50;
+    right: 0; top: 0;
+    background: url(res/teaser.png) no-repeat right top;
+    text-indent: -9999px;
+}
+</style>
 </head>
 <body>
+
+<div id="pageflip">
+    <a href="#">
+        <img src="res/page_flip.png" alt="" />
+        <span class="msg_block">monotone 1.0 is landing soon...</span>
+    </a>
+</div>
 
 <div id="header">
 <p>
