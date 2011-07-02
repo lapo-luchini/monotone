b05e6e69cb8f1de970fd4f34f02f52f2882041aa
Thomas Keller <me@thomaskeller.biz>
2011-07-02 21:08:37 +0000

    Set a proper content type and encoding after all.

--- header.inc.php
+++ header.inc.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html lang="en" xml:lang="en"  xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>monotone <?php echo isset($page_title) ? " : $page_title" : "" ?></title>
 <link type="text/css" rel="stylesheet" href="res/styles.css" />
 <!--[if IE 7]>
