68e6e01a38dbd524e23ab6b7012ebe9d575ee62f
Lapo Luchini <lapo@lapo.it>
2023-02-05 22:03:12 +0000

    Drop old `__float128` work-around, breaks on recent Clang.
    Tested on:
    
    FreeBSD clang version 13.0.0 (git@github.com:llvm/llvm-project.git llvmorg-13.0.0-0-gd7b669b3a303)
    Target: x86_64-unknown-freebsd13.1

--- src/base.hh
+++ src/base.hh
@@ -39,11 +39,6 @@
 #undef __STRICT_ANSI__
 #endif
 
-// Clang doesn't support __float128, but glibc needs it.
-#ifdef __clang__
-struct __float128;
-#endif
-
 #include <iosfwd>
 #include <string>  // it would be nice if there were a <stringfwd>
 
